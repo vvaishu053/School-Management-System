1adec552147522d3e79ddf5d76a165301adad6d8
Vaishnavi K <110079622+vvaishu053@users.noreply.github.com>
2024-07-02 19:30:42 +0530

    Update StudentSideBar.js

--- src/pages/student/StudentSideBar.js
+++ src/pages/student/StudentSideBar.js
@@ -5,7 +5,7 @@ import { Link, useLocation } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-// import AnnouncementOutlinedIcon from '@mui/icons-material/AnnouncementOutlined';
+
 import ClassOutlinedIcon from '@mui/icons-material/ClassOutlined';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 
@@ -32,12 +32,7 @@ const StudentSideBar = () => {
                     </ListItemIcon>
                     <ListItemText primary="Attendance" />
                 </ListItemButton>
-                {/* <ListItemButton component={Link} to="/Student/complain">
-                    <ListItemIcon>
-                        <AnnouncementOutlinedIcon color={location.pathname.startsWith("/Student/complain") ? 'primary' : 'inherit'} />
-                    </ListItemIcon>
-                    <ListItemText primary="Complain" />
-                </ListItemButton> */}
+               
             </React.Fragment>
             <Divider sx={{ my: 1 }} />
             <React.Fragment>
