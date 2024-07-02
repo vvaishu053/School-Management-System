6b067e7955f181006037f7eee830686233035df5
Vaishnavi K <110079622+vvaishu053@users.noreply.github.com>
2024-07-02 20:05:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-// const bodyParser = require("body-parser")
+
 const app = express()
 const Routes = require("./routes/route.js")
 
@@ -10,19 +10,12 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config();
 
-// app.use(bodyParser.json({ limit: '10mb', extended: true }))
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+
 
 app.use(express.json({ limit: '10mb' }))
 app.use(cors())
 
-// mongoose
-//     .connect(process.env.MONGO_URL, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })
-//     .then(console.log("Connected to MongoDB"))
-//     .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
 app.use('/', Routes);
 
 app.listen(PORT, () => {
